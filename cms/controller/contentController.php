e2558b68e6d3efc1f3745226e71a34c6c576aa56
Neale Wolfson <wolfie@digitaltapestries.com>
2014-04-29 09:55:28 +1000

    Added fix to angular and links issue *sigh* Moved to different query to fix bugs

--- cms/controller/contentController.php
+++ cms/controller/contentController.php
@@ -1,5 +1,32 @@
 <?php
 class Cms_ContentController extends Cms_AbstractController{
+    public function getPDFAction(){
+        if (empty($this->_requestParams['pdf'])){
+            return "No file found";
+        }
+        switch ($this->_requestParams['pdf']){
+            case 1:
+                $fileName = 'Another Bloody Murder in Melbourne.pdf';
+                break;
+            case 2:
+                $fileName = 'Australian Sex Party - A brief history.pdf';
+                break;
+
+        }
+        header('Content-Description: File Transfer');
+        header('Content-Type: application/octet-stream');
+        header("Content-Type: application/force-download");
+        header('Content-Disposition: attachment; filename=' . urlencode(basename($fileName)));
+        // header('Content-Transfer-Encoding: binary');
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+        header('Pragma: public');
+        header('Content-Length: ' . 'contentPages/' . filesize($fileName));
+        ob_clean();
+        flush();
+        readfile('contentPages/'.$fileName);
+        exit;
+    }
     public function getPageByIdAction(){
         
         return "Completed getpage";
